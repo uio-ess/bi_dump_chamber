227c92b121b87b7c7470037f7d6d5351c9a064aa
greyltc-uio <greyson.christoforo@fys.uio.no>
2020-08-27 11:18:13 +0200

    snapshot of wip

--- mirrorMounts/3d_assembly.py
+++ mirrorMounts/3d_assembly.py
@@ -5,8 +5,10 @@ import copy
 
 asy=[]
 
+
 # the plate
 plate_thickness = 15
+post_y_offset_from_center = 145
 plate_dxf_filename = 'plate_layout.dxf'
 plate2d = cq.importers.importShape(cq.importers.ImportTypes.DXF, plate_dxf_filename, ignored_layers=['dims'])
 tmp = plate2d.faces().wires()
@@ -16,14 +18,6 @@ plate_x = plate.findSolid().BoundingBox().xlen
 plate_y = plate.findSolid().BoundingBox().ylen
 plate = plate.translate((-plate_x/2,-plate_y/2,-plate_thickness))
 asy.extend(plate.vals())
-
-# post
-post_y_offset_from_center = 145
-post_filename = 'P200_M-Step.step'
-post = cq.importers.importShape(cq.importers.ImportTypes.STEP, post_filename)
-post = post.rotateAboutCenter(axisEndPoint=(1,0,0),angleDegrees=90)
-post_z = post.findSolid().BoundingBox().zlen
-post = post.translate((0,post_y_offset_from_center,post_z/2))
 
 # post base clamp
 clamp_filename = "PF175B-Step.step"
@@ -39,10 +33,31 @@ base = cq.importers.importShape(cq.importers.ImportTypes.STEP, base_filename)
 base = base.rotate((0,0,0), (1,0,0), angleDegrees=90)
 base_thickness = 6.1
 base = base.translate((0,post_y_offset_from_center,base_thickness))
-post = post.translate((0,0,base_thickness))
 asy.extend(base.vals())
-asy.extend(post.vals())
 asy2 = copy.copy(asy) # copy for top
+
+# post (for top)
+post_filename = 'P250_M-Step.step'
+post = cq.importers.importShape(cq.importers.ImportTypes.STEP, post_filename)
+post = post.rotateAboutCenter(axisEndPoint=(1,0,0),angleDegrees=90)
+#post_z = post.findSolid().BoundingBox().zlen
+post = post.translate((0,0,base_thickness))
+post = post.translate((0,post_y_offset_from_center,post.findSolid().BoundingBox().zlen/2))
+asy2.extend(post.vals())
+
+# camera pipe
+# pipe outer = 125
+# pipe inner = 120
+#pipe_length =
+
+# post2 (shorter, for bottom)
+post2_filename = 'P75_M-Step.step'
+post2 = cq.importers.importShape(cq.importers.ImportTypes.STEP, post2_filename)
+post2 = post2.rotateAboutCenter(axisEndPoint=(1,0,0),angleDegrees=90)
+post2 = post2.translate((0,0,base_thickness))
+post2_z = post2.findSolid().BoundingBox().zlen
+post2 = post2.translate((0,post_y_offset_from_center,post2_z/2))
+asy.extend(post2.vals())
 
 # polaris 45
 polaris45_filename = "POLARIS-MA45_M-Step.step"
@@ -56,7 +71,7 @@ polaris45_z = polaris45.findSolid().BoundingBox().zlen
 polaris45 = polaris45.translate((0,0,polaris45_stepfile_base_offset))
 mirror_angle_from_vertical = 0
 polaris45 = polaris45.rotate((0,0,0), (0,0,1), angleDegrees=90+mirror_angle_from_vertical)
-polaris45 = polaris45.translate((0,post_y_offset_from_center,base_thickness+post_z))
+polaris45 = polaris45.translate((0,post_y_offset_from_center,base_thickness+post2_z))
 asy.extend(polaris45.vals())
 
 bot_plate = cq.Compound.makeCompound(asy)
@@ -72,7 +87,7 @@ polaris_base_face_to_mounting_face = 25.3
 polaris_mount_offset_from_center = (-7,-7,polaris_base_face_to_mounting_face+polaris45_angled_face_offset)
 mirror_ass.extend(polaris.vals())
 
-# mirror holder
+# bottom mirror holder
 mirror_mount_filename = "Mirror_Assembly_M9_B.stp"
 mm = cq.importers.importShape(cq.importers.ImportTypes.STEP, mirror_mount_filename)
 mirror_ass.extend(mm.vals())
@@ -83,8 +98,8 @@ bot_mirror_cpnd = bot_mirror_cpnd.translate(polaris_mount_offset_from_center)
 bot_mirror_cpnd = bot_mirror_cpnd.rotate((0,0,0), (0,0,1), angleDegrees=45)
 bot_mirror_cpnd = bot_mirror_cpnd.translate((0,-polaris45_mount_hole_offset,0))
 bot_mirror_cpnd = bot_mirror_cpnd.rotate((0,0,0), (1,0,0), angleDegrees=-45)
-bot_mirror_cpnd = bot_mirror_cpnd.translate((0,post_y_offset_from_center,base_thickness+post_z+polaris45_stepfile_base_offset))
-
+bot_mirror_cpnd = bot_mirror_cpnd.rotate((0,0,0), (0,0,1), angleDegrees=mirror_angle_from_vertical) 
+bot_mirror_cpnd = bot_mirror_cpnd.translate((0,post_y_offset_from_center,base_thickness+post2_z+polaris45_stepfile_base_offset))
 
 top_plate = cq.Compound.makeCompound(asy2)
 show_object(top_plate)
@@ -94,7 +109,7 @@ tm_asy =[]
 clamp_filename = "C1545_M-Step.step"
 clamp = cq.importers.importShape(cq.importers.ImportTypes.STEP, clamp_filename)
 clamp = clamp.rotate((0,0,0), (1,0,0), angleDegrees=-90)
-clamp_along_post = 170
+clamp_along_post = 190
 clamp_plate_face_offset = 38.1
 clamp = clamp.translate((0,post_y_offset_from_center,clamp_along_post))
 tm_asy.extend(clamp.vals())
